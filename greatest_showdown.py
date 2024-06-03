44e62deb77280f51907e7b21fa83cacf6141231c
areiahna <areiahnacooks@yahoo.com>
2024-06-03 00:00:54 -0400

    Corrected if statemnets

--- greatest_showdown.py
+++ greatest_showdown.py
@@ -9,11 +9,11 @@ num2 = int(input("Enter another number: "))
 num3 = int(input("Last one, I promise!: "))
 
 
-if (num1 > num2 and num3):
+if (num1 > num2) and (num1 > num3):
     print("The largest number is", num1)
-elif (num2 > num1 and num3):
+elif (num2 > num1) and (num2 > num3):
     print("The largest number is", num2)
-elif (num3 > num1 and num2):
+elif (num3 > num1) and (num3 > num2):
     print("The largest number is", num3)
 else:
     print("Duplicate numbers??")
